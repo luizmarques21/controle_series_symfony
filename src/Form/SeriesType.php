5985a652247bbf95663e00b79649ed992b2e396e
Luiz Marques <mariel.marques21@gmail.com>
2025-03-24 20:52:58 -0300

    Validaçãoes

--- src/Form/SeriesType.php
+++ src/Form/SeriesType.php
@@ -14,7 +14,9 @@ class SeriesType extends AbstractType
     {
         $builder
             ->add('name', options: ['label' => 'Nome:'])
-            ->add('save', SubmitType::class, ['label' => 'Adicionar'])
+            ->add('save', SubmitType::class, ['label' => $options['is_edit'] ? 'Editar' : 'Adicionar'])
+            ->setMethod($options['is_edit'] ? 'PATCH' : 'POST')
+            ->setAction($options['is_edit'] ? 'app_store_series_changes' : 'app_add_series')
         ;
     }
 
@@ -22,6 +24,9 @@ class SeriesType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Series::class,
+            'is_edit' => false
         ]);
+
+        $resolver->setAllowedTypes('is_edit', ['boolean']);
     }
 }
