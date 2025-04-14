92232e38974b95b5050de4a0f27f656b1298f944
Luiz Marques <mariel.marques21@gmail.com>
2025-04-13 22:24:02 -0300

    Testes

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ tests/Entity/SeasonTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Episode;
+use App\Entity\Season;
+use PHPUnit\Framework\TestCase;
+
+class SeasonTest extends TestCase
+{
+    public function testGetWatchedEpisodes(): void
+    {
+        $season = new Season(1);
+
+        $episode1 = new Episode(1);
+        $episode1->setWatched(true);
+
+        $episode2 = new Episode(2);
+
+        $season->addEpisode($episode1);
+        $season->addEpisode($episode2);
+
+        $watchedEpisodes = $season->getWatchedEpisodes();
+
+        self::assertCount(1, $watchedEpisodes);
+        self::assertSame($episode1, $watchedEpisodes->first());
+    }
+}
