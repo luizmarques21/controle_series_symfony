9e86d01d0a76788e483bb103b45bbc2c5ab5753e
Luiz Marques <mariel.marques21@gmail.com>
2025-05-09 20:02:18 -0300

    Pacotes uteis

--- src/MessageHandler/DeleteSeriesImageHandler.php
+++ src/MessageHandler/DeleteSeriesImageHandler.php
@@ -4,19 +4,20 @@ namespace App\MessageHandler;
 
 use App\Message\SeriesWasDeleted;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class DeleteSeriesImageHandler
 {
-    public function __construct(private ParameterBagInterface $parameterBag)
+    public function __construct(private ParameterBagInterface $parameterBag, private Filesystem $filesystem)
     {
     }
 
     public function __invoke(SeriesWasDeleted $message)
     {
         $coverImagePath = $message->series->getCoverImagePath();
-        unlink(
+        $this->filesystem->remove(
             $this->parameterBag->get('cover_image_directory') . DIRECTORY_SEPARATOR . $coverImagePath
         );
     }
